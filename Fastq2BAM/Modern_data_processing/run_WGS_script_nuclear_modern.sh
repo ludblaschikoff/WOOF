b7e94497f8aa409b2e79ed9930b805ee77683488
Ludmilla Paixão Blaschikoff <85697773+ludblaschikoff@users.noreply.github.com>
2022-08-08 13:07:23 +0100

    Update run_WGS_script_nuclear_modern.sh

--- Fastq2BAM/Modern_data_processing/run_WGS_script_nuclear_modern.sh
+++ Fastq2BAM/Modern_data_processing/run_WGS_script_nuclear_modern.sh
@@ -7,18 +7,14 @@ echo '
 2nd argument is the number of threads available to use. example "15"
 3rd argument is the complete path to the directory were results must be saved
 4th argument is the sample name
-5th argument is the minimum snp coverage. example 7
-6th argument is the minimum snp quality. example 30
-7th argument is the minimum genotype quality. example 20
-8th argument is the name of the snpEff database (ex: canis_mt or CanFam3.1.95)
-9th argument is the biosample name
-10th argument is the minimum mapping quality. example "20"
-11th argument is the library name'
+5th argument is the biosample name
+6th argument is the minimum mapping quality. example "20"
+7th argument is the library name'
 }
 
 
 #check if required arguments are there and display usage message
-if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ] || [ -z "$5" ] || [ -z "$6" ] || [ -z "$7" ] || [ -z "$8" ] || [ -z "$9" ] || [ -z "$10" ] || [ -z "$11" ]; then
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ] || [ -z "$5" ] || [ -z "$6" ] || [ -z "$7" ]; then
         printf "Please provide the arguments required for the script.\n\n"
         display_usage
         exit 1
@@ -30,13 +26,9 @@ reference="$1"
 threads="$2"
 base_output="$3"
 base_name="$4"
-snp_coverage="$5"
-snp_quality="$6"
-genotype_quality="$7"
-snpEffDB="$8"
-biosample="$9"
-minmapqual="${10}"
-library_name="${11}"
+biosample="$5"
+minmapqual="$6"
+library_name="$7"
 
 
 #########################
@@ -90,25 +82,3 @@ rm $base_name'.sam' $base_name'_dog_mappedandunmapped.bam' $base_name'.bam' $bas
 
 qualimap bamqc -nt "$threads" -c -bam "$base_name"_sorted_no_dups_.bam --java-mem-size=32G
  
-#########################
-#### VARIANT CALLING ####
-#########################
-
-mkdir $base_output'/variant_calling'
-
-gatk HaplotypeCaller -R "$reference" -I "$base_name"_sorted_no_dups_.bam --bam-output "$base_name"_GATK_out.bam -O "$base_output"/variant_calling/"$base_name".vcf
-
-#vcf filter using GATK
-
-cd $base_output'/variant_calling'
-gatk VariantFiltration -R "$reference" -V "$base_output"/variant_calling/"$base_name".vcf --filter-name 'FAILED_qual' --filter-expression "QD < $snp_quality" --genotype-filter-name 'FAILED_DP' --genotype-filter-expression "DP < $snp_coverage" --genotype-filter-name "FAILED_GQ" --genotype-filter-expression "GQ < $genotype_quality" --set-filtered-genotype-to-no-call true -O "$base_output"/variant_calling/"$base_name"_filtered.vcf
-
-gatk SelectVariants -V "$base_output"/variant_calling/"$base_name"_filtered.vcf --exclude-filtered true --select-type-to-exclude INDEL --exclude-non-variants true --remove-unused-alternates true --restrict-alleles-to BIALLELIC -O "$base_output"/variant_calling/"$base_name"_PASS_ONLY_noINDELS.vcf
-
-gatk SelectVariants -V "$base_output"/variant_calling/"$base_name"_filtered.vcf --exclude-filtered true --exclude-non-variants true --remove-unused-alternates true --restrict-alleles-to BIALLELIC -O "$base_output"/variant_calling/"$base_name"_PASS_ONLY_INDELS.vcf
-
-########################
-###### SNP effects #####
-########################
-
-java -jar /opt/anaconda3/share/snpeff-4.3.1t-1/snpEff.jar "$snpEffDB" "$base_output"/variant_calling/"$base_name"_PASS_ONLY_noINDELS.vcf > "$base_output"/variant_calling/"$base_name"_effects_noINDELS.vcf
