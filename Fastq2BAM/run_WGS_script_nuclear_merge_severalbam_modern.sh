61bc7bb8931cf54f8745a0612c4b1e278870223b
Ludmilla Paixão Blaschikoff <85697773+ludblaschikoff@users.noreply.github.com>
2023-03-09 12:48:31 +0000

    Update and rename Fastq2BAM/DOGPT12-79/run_WGS_script_nuclear_merge_severalbam_modern.sh to Fastq2BAM/run_WGS_script_nuclear_merge_severalbam_modern.sh

--- Fastq2BAM/run_WGS_script_nuclear_merge_severalbam_modern.sh
+++ Fastq2BAM/run_WGS_script_nuclear_merge_severalbam_modern.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-#Script to run entire aDNA analysis at once
+#Script to merge several bam files for samples with multiple sequencing runs.
+
+#USAGE: bash run_WGS_script_nuclear_merge_severalbam_modern.sh path/bamfiles threads path/output samplename
 
 display_usage() {
 echo '
 1th argument is the path to the folder where are bams file to merge
 2th argument is the number of threads available to use. example "15"
 3th argument is the complete path to the directory were results must be saved
-4th argument is the sample name'
+4th argument is the sample name. ex: biosample name'
 }
 
 
@@ -18,14 +20,14 @@ if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ] ; then
 fi
 
 
-#variantes 
+#############################
+######### VARIABLES #########
+#############################
+
 path_bams="$1"
 threads="$2"
 base_output="$3"
 base_name="$4"
-
-
-
 
 
 #########################
@@ -34,10 +36,13 @@ base_name="$4"
 
 cd $base_output
 
-samtools merge "$base_name".bam "$path_bams"*.bam
+samtools merge --threads "$threads" "$base_name".bam "$path_bams"*.bam
 
 samtools sort "$base_name".bam > "$base_name"_sorted.bam
 samtools index "$base_name"_sorted.bam
 
+rm "$base_name".bam
+
 qualimap bamqc -nt "$threads" -c -bam "$base_name"_sorted.bam --java-mem-size=32G
 
+echo "Done!"
