36dce56b25e0fa00d71bbb202327d7880d24701e
Ludmilla Paixão Blaschikoff <85697773+ludblaschikoff@users.noreply.github.com>
2023-03-09 19:04:55 +0000

    Update run_WGS_script_BQSR.sh

--- Chalcolithic_Paper/Modern/run_WGS_script_BQSR.sh
+++ Chalcolithic_Paper/Modern/run_WGS_script_BQSR.sh
@@ -26,15 +26,15 @@ base_output="$3"
 maxRAM="$4"				
 
 
-#CALCULAMOS A RAM DISPONIVEL PARA CADA RUN
+#WE CALCULATE THE AVAILABLE RAM FOR EACH RUN
 
 threads=30
-ind_ram=$(( $maxRAM/$threads ))		#run é o numero de amostras a serem processadas. é o mesmo que threads.
+ind_ram=$(( $maxRAM/$threads ))		#run is the number of samples to be processed. it's the same as threads.
 
-ind_ram=$ind_ram'g'					##atualizo o valor da variavel $ind_ram para $ind_ram + g para o nome da variável levar a letra g.
+ind_ram=$ind_ram'g'					
 
 #### BaseRecalibrator ####
 
-ls $bam_files_dir/*.bam | xargs -n 1 -P $threads -I {} sh -c 'base_name="$(basename {} | cut -d_ -f1)"; echo "$base_name"; gatk --java-options "-Xmx'$ind_ram'" BaseRecalibrator -R '$reference' -I {} --known-sites /mnt/storage1/DATA/canis/SNP_array/variants_known_sites/All.vcf.gz -O "'$base_output'"/"$base_name"_repreprocessing/NUCLEAR/mapping/recal_data_"$base_name".table ;  gatk --java-options -Xmx4G ApplyBQSR -R '$reference' -I {} --bqsr-recal-file "'$base_output'"/"$base_name"_repreprocessing/NUCLEAR/mapping/recal_data_"$base_name".table -O "'$base_output'"/"$base_name"_repreprocessing/NUCLEAR/mapping/BQSR_"$base_name"_no_dups_sorted.bam ' 		 # -P $runs é o numero de processadores que distribuo para cada amostra; #xargs (short for "eXtended ARGuments") serve para correr comandos em paralelo. is a command on Unix and most Unix-like operating systems used to build and execute commands from standard input. It converts input from standard input (ficheiros) into arguments to a command.
+ls $bam_files_dir/*.bam | xargs -n 1 -P $threads -I {} sh -c 'base_name="$(basename {} | cut -d_ -f1)"; echo "$base_name"; gatk --java-options "-Xmx'$ind_ram'" BaseRecalibrator -R '$reference' -I {} --known-sites /mnt/storage1/DATA/canis/SNP_array/variants_known_sites/All.vcf.gz -O "'$base_output'"/"$base_name"_repreprocessing/NUCLEAR/mapping/recal_data_"$base_name".table ;  gatk --java-options -Xmx4G ApplyBQSR -R '$reference' -I {} --bqsr-recal-file "'$base_output'"/"$base_name"_repreprocessing/NUCLEAR/mapping/recal_data_"$base_name".table -O "'$base_output'"/"$base_name"_repreprocessing/NUCLEAR/mapping/BQSR_"$base_name"_no_dups_sorted.bam ' 		 # -P $runs is the number of processors I assign to each sample; #xargs (short for "eXtended ARGuments") is for running commands in parallel. is a command on Unix and most Unix-like operating systems used to build and execute commands from standard input. It converts input from standard input (files) into arguments to a command.
 
 	
